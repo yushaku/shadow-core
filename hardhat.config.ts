19d592e223d62439be327440325b3666453d26c0
SonLV <son@nami.foundation>
2025-08-12 09:44:13 +0700

    🔧 (chore): Add configuration

---
+++ hardhat.config.ts
@@ -0,0 +1,41 @@
+import "@nomicfoundation/hardhat-toolbox-viem";
+import "@nomicfoundation/hardhat-verify";
+import "@nomicfoundation/hardhat-foundry";
+import "@openzeppelin/hardhat-upgrades";
+import "hardhat-deploy";
+import "hardhat-gas-reporter";
+import type { HardhatUserConfig } from "hardhat/config";
+
+const accounts = process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [];
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.28",
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    },
+    ethereum: {
+      url: "https://eth.llamarpc.com",
+      accounts,
+    },
+    sepolia: {
+      url: "https://ethereum-sepolia-rpc.publicnode.com",
+      accounts,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: false,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    coinmarketcap: "",
+  },
+  namedAccounts: {
+    deployer: { default: 0 }
+  },
+};
+
+export default config;
